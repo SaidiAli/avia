98328858bbf6270fc029bdc20d42ba496f21793c
isiko saidiali <isikosaidiali@gmail.com>
2023-10-01 22:39:11 +0300

    ll

--- resources/views/layouts/footers/guest/description.blade.php
+++ resources/views/layouts/footers/guest/description.blade.php
@@ -2,25 +2,25 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mb-4 mx-auto text-center">
-                <a href="https://www.creative-tim.com" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                <a href="#" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Company
                 </a>
-                <a href="https://www.creative-tim.com/presentation" target="_blank"
+                <a href="#" target="_blank"
                     class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     About Us
                 </a>
-                <a href="https://www.creative-tim.com/presentation#pablo" target="_blank"
+                <a href="#" target="_blank"
                     class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Team
                 </a>
-                <a href="https://www.creative-tim.com/templates" target="_blank"
+                <a href="#" target="_blank"
                     class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Products
                 </a>
-                <a href="https://www.creative-tim.com/blog" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                <a href="#" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Blog
                 </a>
-                <a href="https://www.creative-tim.com/support-terms" target="_blank"
+                <a href="#" target="_blank"
                     class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                     Pricing
                 </a>
